6ae08f1d55c51d5f5074e4bbe49c4adb929493b9
AbdulMaaz909 <abdulmaaz827@gmail.com>
2025-10-02 10:27:43 +0530

    make changes in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    outputFileTracingRoot: "C:\\Users\\abdul\\Portolio\\portfilio",
+    outputFileTracingRoot: "C:\\Users\\abdul\\Portfolio\\portfilio",
 };
 
 export default nextConfig;
