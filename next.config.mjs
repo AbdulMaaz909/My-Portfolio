800c4ddb0533094d0c5b448310c790693bcde1d6
AbdulMaaz909 <abdulmaaz827@gmail.com>
2025-10-02 10:20:26 +0530

    added correct in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    outputFileTracingRoot: "C:\\Users\\abdul\\portfilio",
+    outputFileTracingRoot: "C:\\Users\\abdul\\Portolio\\portfilio",
 };
 
 export default nextConfig;
