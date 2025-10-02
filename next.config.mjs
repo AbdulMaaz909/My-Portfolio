71b076f2b2820f9324e31f59adc990b13f6e9364
AbdulMaaz909 <abdulmaaz827@gmail.com>
2025-10-02 10:39:19 +0530

    Fix outputFileTracingRoot for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     outputFileTracingRoot: "C:\\Users\\abdul\\Portfolio\\portfilio",
-// };
-
-// export default nextConfig;
-
-
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
-}
+    outputFileTracingRoot: "C:\Users\abdul\Portfolio\portfilio",
+    // C:\Users\abdul\Portfolio\portfilio
+};
 
-export default nextConfig
+export default nextConfig;
+
+
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     outputFileTracingRoot: path.join(__dirname, '../../'),
+//   },
+// }
+
+// export default nextConfig
